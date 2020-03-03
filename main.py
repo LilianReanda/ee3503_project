9be80a2d19fdd646dd26ef6ff6b150dcf8a8b506
LilianReanda <60326719+LilianReanda@users.noreply.github.com>
2020-03-02 20:09:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from urllib.request import urlopen
+from io import StringIO
+import csv
+
+data = urlopen("http://www.sharecsv.com/s/c6ecf2e89c3b1472614295b4a647e4f6/GuatED.csv").read().decode('ascii','ignore')
+datafile = StringIO(data)
+csvReader = csv.reader(datafile)
+
+for row in csvReader:
+  print(row)
+
