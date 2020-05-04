11c2e70c028319a146dec6b6dd52d101520e4fb5
LilianReanda <60326719+LilianReanda@users.noreply.github.com>
2020-05-04 12:04:28 -0500

    commit

--- data-gathering/main.py
+++ data-gathering/main.py
@@ -2,8 +2,13 @@ from urllib.request import urlopen
 from io import StringIO
 import csv
 
-data = urlopen("http://www.sharecsv.com/s/c6ecf2e89c3b1472614295b4a647e4f6/GuatED.csv").read().decode('ascii','ignore')
+def datal
+
+datal = urlopen("http://www.sharecsv.com/s/c6ecf2e89c3b1472614295b4a647e4f6/GuatED.csv").read().decode('ascii','ignore')
 datafile = StringIO(data)
 csvReader = csv.reader(datafile)
 
+if datal == '__main__':
+    main()
+    process.process()
 
